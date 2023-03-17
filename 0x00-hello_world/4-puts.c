53e2b56274f342be1db2ad2ea09cb4135fa07310
Lthekidd <labriny04@gmail.com>
2023-03-16 17:53:26 -0700

    gg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+i#include <stdio.h>
 /**
-*main - First c file
-*return: always 0 (success)
+*main - main entry point
+*return : always 0 (success)
 */
 int main(void)
 {
-	puts( "\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
