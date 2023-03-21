820de538ddead65a6e83c4ee7adb6ea5076d80de
LThekidd <Labriny04@gmail.com>
2023-03-21 14:02:52 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * main - prints alphabet in lower case
+  * followered by a new line
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
