c133ede3249d0f196897dd10552a787cd46c5d5e
LThekidd <Labriny04@gmail.com>
2023-03-21 15:07:20 +0000

    gg

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+   * main - check the code.
+    *
+     * Return: Always 0.
+      */
+void print_alphabet_x10(void)
+{
+	char ch;
+
+	int n;
+
+	n = 0;
+	ch = 'a';
+
+	while (n <= 10)
+	{
+		while (ch <= 'z')
+			_putchar(ch)
+			ch++;
+		a++;
+	}
+	_putchar('\n');
+}
