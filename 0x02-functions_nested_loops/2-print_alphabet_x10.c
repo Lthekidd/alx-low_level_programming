d0b6208e08430299642147af1f2060fb4217e4a5
LThekidd <Labriny04@gmail.com>
2023-03-21 15:37:19 +0000

    gg

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,6 +13,7 @@ void print_alphabet_x10(void)
 
 	while (n <= 10)
 	{
+		
 		ch = 'a';
 
 		while (ch <= 'z')
@@ -21,6 +22,7 @@ void print_alphabet_x10(void)
 			ch++;
 		}
 		n++;
+	
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
