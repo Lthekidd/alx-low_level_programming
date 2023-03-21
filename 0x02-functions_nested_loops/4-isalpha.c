2115249b44833bee8d560b6e14c044bc90bab5eb
LThekidd <Labriny04@gmail.com>
2023-03-21 16:17:42 +0000

    gg

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if c is lower.
+ * @c : character the code is going to check
+ * Return: 1 or 0 .
+ */
+int _isalpha(int c)
+{
+	return (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z');
+}
